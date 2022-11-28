fd228f174159f391ca9a802f2d85f6593e3d5e34
Narinder <narinderv@gmail.com>
2022-11-28 17:50:46 +0530

    Modified test file

--- test_hello.py
+++ test_hello.py
@@ -1,25 +1,19 @@
 
 from hello import add, subtract, greet
 
-x = 10
-y = 6
 
-def setup_function():
-    print("Test Setup...\n")
+def test_add():
     x = 10
     y = 6
-
-def teardown_function():
-    print("Test Teardown...\n")
-
-def runTests():
     print("Addition Test:\n")
-    #print("Add: ", add(x,y))
     assert add(x, y) == 16
 
+def test_subtract():
+    x = 8
+    y = 4
     print("Subtraction Test:\n")
     assert subtract(x, y) == 4
 
-setup_function()
-runTests()
-teardown_function()
+#setup_function()
+#runTests()
+#teardown_function()
